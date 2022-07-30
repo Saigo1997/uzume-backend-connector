c7ce9824a3eae8ce02ba287dbb9be6dbc7b8b77a
Saigo1997 <a.saigo1997@gmail.com>
2022-07-31 00:51:32 +0900

    backend version修正

--- backendConnector.ts
+++ backendConnector.ts
@@ -18,7 +18,7 @@ class BackendStatus {
 export class BackendConnector {
   // バージョン設定
   private static backendMinorVersion = '0.0'
-  static latestBackendVersion = `${BackendConnector.backendMinorVersion}.3`
+  static latestBackendVersion = `${BackendConnector.backendMinorVersion}.4`
 
   static loggingEnable = true
   static backendStatus = BackendStatus.INIT
